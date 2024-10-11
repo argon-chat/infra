c517bcb73935365c587efe703576f3e1047575f6
Aram <aramhrptn@hotmail.com>
2024-10-12 03:36:21 +0400

    Add .gitignore and setup_docker.sh script

---
+++ scripts/setup_docker.sh
@@ -0,0 +1,27 @@
+if ! command -v docker &> /dev/null
+then
+  sudo apt-get update
+  sudo apt-get install ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+  # make docker rootless
+else
+  echo "Docker is already installed"
+fi
+# check if docker is accessible without root
+if ! docker run hello-world &> /dev/null
+then
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  newgrp docker
+fi
+docker run hello-world || exit 64
